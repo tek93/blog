95f1b0366fc7bd1b74dac072b0331f434d57039b
tek93 <=>
2020-05-31 01:39:14 +0200

    updates to deploy to heroku

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/blog'));
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/blog/index.html'));});
 app.listen(process.env.PORT || 8080);
